d4d608aee22053a1a1928b67d6195764795288e1
Natnael31 <end24002byui.edu>
2024-09-27 16:41:00 +0300

    picture album assignment

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -5,4 +5,4 @@ let year = document.getElementById("currentyear");
 year.textContent = `©${currentDate.getFullYear()}`;
 
 let date = document.getElementById("lastModified");
-date.textContent = `Last Modification : ${lastModified.getMonth()}/${lastModified.getDate()}/${lastModified.getFullYear()} ${lastModified.getHours()}:${lastModified.getMinutes()}:${lastModified.getSeconds()}`
+date.textContent = `Last Modification : ${lastModified.getMonth() + 1}/${lastModified.getDate()}/${lastModified.getFullYear()} ${lastModified.getHours()}:${lastModified.getMinutes()}:${lastModified.getSeconds()}`
